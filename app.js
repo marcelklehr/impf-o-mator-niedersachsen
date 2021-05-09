88c8340022e24277fbae34aed96ca70c4214f777
Marcel Klehr <mklehr@gmx.net>
2021-05-09 18:06:07 +0200

    Let there be :bulb:

---
+++ app.js
@@ -0,0 +1,127 @@
+const feathers = require('@feathersjs/feathers');
+const express = require('@feathersjs/express');
+const socketio = require('@feathersjs/socketio');
+const Parallel = require('async-parallel')
+
+class RegionService {
+    constructor() {
+        this.regions = [];
+    }
+
+    async find () {
+        return this.regions;
+    }
+
+    async get(id) {
+        return this.regions.find(region => region.id === id)
+    }
+
+    async create (data) {
+        const region = {
+            id: data.id,
+            count: 1,
+            result: null
+        }
+
+        // Add new message to the list
+        this.regions.push(region);
+
+        return region;
+    }
+
+    async update (id, data) {
+        const regionIdx = this.regions.findIndex(region => region.id === id)
+        this.regions[regionIdx] = {...this.regions[regionIdx], ...data, id: this.regions[regionIdx].id}
+        return this.regions[regionIdx]
+    }
+}
+
+// Creates an ExpressJS compatible Feathers application
+const app = express(feathers());
+
+// Parse HTTP JSON bodies
+app.use(express.json());
+// Parse URL-encoded params
+app.use(express.urlencoded({ extended: true }));
+// Host static files from the current folder
+app.use(express.static(__dirname+'/public'));
+// Add REST API support
+app.configure(express.rest());
+// Configure Socket.io real-time APIs
+app.configure(socketio());
+// Register an in-memory messages service
+app.use('regions', new RegionService());
+// Register a nicer error handler than the default Express one
+app.use(express.errorHandler());
+
+// Add any new real-time connection to the `everybody` channel
+app.on('connection', connection =>
+    app.channel('everybody').join(connection)
+);
+// Publish all events to the `everybody` channel
+app.publish(data => app.channel('everybody'));
+
+// Start the server
+app.listen(3030).on('listening', () =>
+    console.log('Feathers server listening on localhost:3030')
+);
+
+const fetch = require('node-fetch')
+const https = require('https')
+const httpsAgent = new https.Agent({
+    keepAlive: true
+});
+
+;(async () => {
+    const regionsService = app.service('regions')
+    do {
+        const regions = await regionsService.find()
+        await Parallel.each(regions, async region => {
+            console.log('Fetching region', region.id)
+            let body
+            try {
+                body = await new Promise(async (resolve, reject) => {
+                    const req = await https.get(
+                        `https://[2001:4d50:100:1e::20]/portal/rest/appointments/findVaccinationCenterListFree/${region.id}?stiko=&count=1`,
+                        {
+                            agent: httpsAgent,
+                            setHost: false,
+                            headers: {
+                                'Host': 'www.impfportal-niedersachsen.de',
+                                'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0',
+                                Accept: 'application/json, text/plain, */*',
+                                'Accept-Language': 'en',
+                                'Authorization': '',
+                                'DNT': '1',
+                                'Referer': 'https://www.impfportal-niedersachsen.de/portal/'
+                            }
+                        },
+                        res => {
+                            res.setEncoding('utf8')
+                            let body = ''
+                            res.on('data', (chunk) => {
+                                body += chunk
+                            })
+                            res.on('end', () => resolve(body))
+                        }
+                    )
+                    req.on('error', reject)
+                })
+            }catch(e) {
+                console.error(e)
+            }
+            try {
+                body = JSON.parse(body)
+            }catch(e) {
+                console.warn('Error parsing response:')
+                console.warn(body)
+                return
+            }
+            console.log(body)
+            if (body.succeeded) {
+                await regionsService.update(region.id, {...region, result: body.resultList})
+            }
+        }, 2)
+        await new Promise(resolve => setTimeout(resolve, 180000))
+    } while(true)
+})()
---
+++ public/client.js
@@ -0,0 +1,125 @@
+// Establish a Socket.io connection
+const socket = io();
+// Initialize our Feathers client application through Socket.io
+// with hooks and authentication.
+const client = feathers();
+
+client.configure(feathers.socketio(socket));
+
+// Setup screen
+const setupHTML = `<main class="setup container">
+<div class="hero is-primary">
+<div class="hero-body">
+  <h1 class="title block">Impf-o-mat Niedersachsen</h1>
+  <div class="subtitle">Der Impf-o-mat informiert Sie, wenn Impftermine in Ihrer Region frei werden.</div>
+</div>
+</div>
+  <div class="section mt-5">
+  <form class="form">
+    <label class="label">Postleitzahl eingeben
+      <input class="block input" type="text" name="id" placeholder="PLZ">
+    </label>
+
+    <button type="button" id="setup" class="button is-primary block signup">
+      Ich will geimpft werden.
+    </button>
+  </form>
+  </div>
+</main>`;
+
+const mainHTML = `<main class="main container">
+<div class="hero is-primary">
+<div class="hero-body">
+  <h1 class="title block">Impf-o-mat Niedersachsen</h1>
+  <div class="subtitle">Der Impf-o-mat informiert Sie, wenn Impftermine in Ihrer Region frei werden.</div>
+</div>
+</div>
+  <div class="section mt-5 results">
+  <h2 class="title">Warte auf Impftermine in <span id="plz"></span></h2>
+  <div class="subtitle">Sie können diese Seite im Hintergrund geöffnet lassen und auf eine Benachrichtigung warten.</div>
+  <progress class="progress is-small is-primary" max="100">0%</progress>
+  </div>
+</main>`;
+
+const showSetup = (error) => {
+    if(document.querySelectorAll('.setup').length && error) {
+        document.querySelector('.heading').insertAdjacentHTML('beforeend', `<p>There was an error: ${error.message}</p>`);
+    } else {
+        document.getElementById('app').innerHTML = setupHTML;
+    }
+};
+
+// Shows the main page
+const showMain = async () => {
+    document.getElementById('app').innerHTML = mainHTML;
+    document.getElementById('plz').innerHTML = Object.keys(subscriptions)[0];
+};
+
+const addNotification = center => {
+    const results = document.querySelector('.results');
+
+    if(results) {
+        results.innerHTML = `<div class="message is-primary">
+<div class="message-header">
+      <h2 class="title">Freie Impftermine in ${center.city} gefunden</h2>
+</div>
+      <div class="message-body">
+        <p>Öffnen Sie jetzt das <a href="https://impfportal-niedersachsen.de">Impfportal Niedersachsen</a>, um einen Termin zu buchen.</p>
+        <a class="button is-primary mt-3">Impfportal öffnen</a>
+      </div>
+    </div>`;
+    }
+    new Notification(`Freie Impftermine in ${center.city} gefunden`)
+};
+
+// Retrieve post code from setup page
+const getSetupData = () => {
+    const region = {
+        id: document.querySelector('[name="id"]').value,
+    };
+
+    return region;
+};
+
+const subscriptions = {}
+const subscribe = async (region) => {
+    subscriptions[region.id] = true
+}
+
+const onUpdate = async (region) => {
+  if (!subscriptions[region.id]) {
+      return
+  }
+  const stockedCenters = region.result.filter(result => !result.outOfStock)
+    stockedCenters.forEach(center => addNotification(center))
+    if (stockedCenters.length) {
+        delete subscriptions[region.id]
+    }
+}
+
+const addEventListener = (selector, event, handler) => {
+    document.addEventListener(event, async ev => {
+        if (ev.target.closest(selector)) {
+            handler(ev);
+        }
+    });
+};
+
+addEventListener('#setup', 'click', async () => {
+    const regionData = getSetupData();
+
+    let region = await client.service('regions').get(regionData.id)
+    if (!region) {
+        region = await client.service('regions').create(regionData)
+    }else{
+        await client.service('regions').update(region.id, {count: region.count+1})
+    }
+
+    await subscribe(region)
+    showMain()
+});
+
+client.service('regions').on('updated', onUpdate);
+
+showSetup()
+window.Notification.requestPermission()
